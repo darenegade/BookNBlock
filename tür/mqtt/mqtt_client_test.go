5ded92193a85ced72dabc7573bc6121faafa357c
Maxl94 <balluff@hm.edu>
2018-05-26 14:09:08 +0200

    Mqtt init

---
+++ tür/mqtt/mqtt_client_test.go
@@ -0,0 +1,36 @@
+package mqtt
+
+import (
+	"sync"
+	"testing"
+
+	mqtt "github.com/eclipse/paho.mqtt.golang"
+)
+
+func TestMock_IsAllowedAt(t *testing.T) {
+	const TOPIC = "test"
+
+	opts := mqtt.NewClientOptions().AddBroker("tcp://104.196.103.14:1883")
+
+	client := mqtt.NewClient(opts)
+	if token := client.Connect(); token.Wait() && token.Error() != nil {
+		t.Fatal(token.Error())
+	}
+
+	var wg sync.WaitGroup
+	wg.Add(1)
+
+	if token := client.Subscribe(TOPIC, 0, func(client mqtt.Client, msg mqtt.Message) {
+		if string(msg.Payload()) != "mymessage" {
+			t.Fatalf("want mymessage, got %s", msg.Payload())
+		}
+		wg.Done()
+	}); token.Wait() && token.Error() != nil {
+		t.Fatal(token.Error())
+	}
+
+	if token := client.Publish(TOPIC, 0, false, "mymessage"); token.Wait() && token.Error() != nil {
+		t.Fatal(token.Error())
+	}
+	wg.Wait()
+}
