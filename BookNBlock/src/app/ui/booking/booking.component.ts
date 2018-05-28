d792948833c7f43638f392051fbfc0c1def13a36
Alexandra <alexandra.wenzel.mail@gmail.com>
2018-05-28 22:08:08 +0200

    MockConnector benutzt (review Kommentar)

--- BookNBlock/src/app/ui/booking/booking.component.ts
+++ BookNBlock/src/app/ui/booking/booking.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Offer } from '../../data/offer';
+import {MockConnector} from '../../connector/mock.connector';
 
 @Component({
   selector: 'app-booking',
@@ -10,63 +11,18 @@ export class BookingComponent implements OnInit {
 
   allOffers: Offer[] = [];
 
-  offer: Offer = {
-    id: 4,
-    doorId: 2222,
-    isBooked: false,
-    prize: 400,
-    fromDate: new Date(2018, 4, 17),
-    toDate: new Date(2018, 4, 21),
-    address: 'Marienplatz 5, 89069 München',
-    name: 'Max Mustermann',
-    walletId: 38383,
-    description: 'Wunderschöne Wohnung in bester Lage',
-    title: 'Wohnen mitten in München',
-    image: null
-  };
-
-  offer2: Offer = {
-    id: 5,
-    doorId: 4677,
-    isBooked: false,
-    prize: 200,
-    fromDate: new Date(2018, 7, 1),
-    toDate: new Date(2018, 7, 14),
-    address: 'Hochkalterstraße 5, 81547 München',
-    name: 'Anna Mustermann',
-    walletId: 646466,
-    description: 'Wohnen im Grünen, gleich in der Nähe der Isar',
-    title: 'Vermiete meine Wohnung wegen Urlaub',
-    image: null
-  };
-
-  offer3: Offer = {
-    id: 5,
-    doorId: 4677,
-    isBooked: false,
-    prize: 200,
-    fromDate: new Date(2018, 7, 1),
-    toDate: new Date(2018, 7, 14),
-    address: 'Hochkalterstraße 5, 81547 München',
-    name: 'Anna Mustermann',
-    walletId: 646466,
-    description: 'Wohnen im Grünen, gleich in der Nähe der Isar',
-    title: 'Vermiete meine Wohnung wegen Urlaub',
-    image: null
-  };
-
-  constructor() {
-    this.allOffers.push(this.offer);
-    this.allOffers.push(this.offer2);
-    this.allOffers.push(this.offer3);
+  constructor(private mockConnecotr: MockConnector) {
   }
 
   ngOnInit() {
     this.getAllOffers();
   }
 
-  getAllOffers(): Offer[] {
-    return this.allOffers;
+  /**
+   * Get all offers.
+   */
+  getAllOffers(): void {
+    this.mockConnecotr.getAllOffers().then(result => this.allOffers = result);
   }
 
 }
