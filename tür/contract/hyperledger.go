87dce8cda39b09bc8a8bab779842034e4786bb68
Mainau <felix.thcilhcs@gmail.com>
2018-06-07 18:35:53 +0200

    added Offer struct, unmarshal method

--- tür/contract/hyperledger.go
+++ tür/contract/hyperledger.go
@@ -1,6 +1,7 @@
 package contract
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"log"
@@ -10,19 +11,34 @@ import (
 
 type (
 	HyperLedger struct {
-		Url string
+		Url   string
+		offer Offer
+	}
+	Offer struct {
+		OfferID    int64     `json:"offerId"`
+		Free       bool      `json:"free"`
+		Price      float64   `json:"price"`
+		CheckIn    time.Time `json:"checkIn"`
+		CheckOut   time.Time `json:"checkOut"`
+		ObjectName string    `json:"objectName"`
+		OwnerName  string    `json:"ownerName"`
+		TenantPk   string    `json:"tenantPk"`
+		LandlordPk string    `json:"landlordPk"`
 	}
 )
 
-//renterID is the users Public Key
-func (h *HyperLedger) isAllowedAt(renterID string, requestPointofTime time.Time, startTime time.Time, endTime time.Time) (allowed bool) {
-
+//renterID is not needed here (in contrast to the equivalent method for ethereum), since decrypting the message with the PK is proof enough for the requester's authenticity
+func (h *HyperLedger) isAllowedAt(requestPointofTime time.Time, startTime time.Time, endTime time.Time) (allowed bool) {
+	if requestPointofTime.Before(endTime) && requestPointofTime.After(startTime) {
+		return true
+	}
 	return false
 }
 
-func (h *HyperLedger) getBlockData() (responseData []byte) {
+func (h *HyperLedger) getBlockData() (offer Offer) {
 	fmt.Println("Starting the application...")
 	response, err := http.Get(h.Url)
+	var responseData []byte
 	if err != nil {
 		fmt.Printf("The HTTP request failed with error %s\n", err)
 	} else {
@@ -32,83 +48,21 @@ func (h *HyperLedger) getBlockData() (responseData []byte) {
 		}
 		fmt.Println(string(responseData))
 	}
-
-	return responseData
+	offer = Offer{} // Slice of Offer instances
+	json.Unmarshal(responseData, &offer)
+	return offer
+}
+func (h *HyperLedger) getRenterID(payload string) {
+	// if h.offer == nil {
+	// 	return
+	// }
 }
 
-package contract
-
-import (
-	"crypto/aes"
-	"crypto/cipher"
-	"encoding/hex"
-	"fmt"
-	"io/ioutil"
-	"log"
-	"net/http"
-	"strconv"
-	"strings"
-	time "time"
-)
-
-type (
-	HyperLedger struct {
-		Url string
-	}
-)
-
-//renterID is the users Public Key
-func (h *HyperLedger) isAllowedAt(renterID string, requestPointofTime time.Time, startTime time.Time, endTime time.Time) (allowed bool) {
-
-	return false
-}
-
-func (h *HyperLedger) getBlockData() (responseData []byte) {
-	fmt.Println("Starting the application...")
-	response, err := http.Get(h.Url)
-	if err != nil {
-		fmt.Printf("The HTTP request failed with error %s\n", err)
-	} else {
-		data, _ := ioutil.ReadAll(response.Body)
-		fmt.Println(string(data))
-	}
-	responseData, err = ioutil.ReadAll(response.Body)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println(string(responseData))
-	return responseData
-}
-
-func (h *Hyperledger) decryptPaylpad(payload string) (renterPK tür.RenterPK, timestamp int64) {
-	key, _ := hex.DecodeString("6368616e676520746869732070617373776f726420746f206120736563726574")
-	ciphertext, _ := hex.DecodeString(payload)
-	nonce := []byte("64a9433eae7c")
-
-	block, err := aes.NewCipher(key)
-	if err != nil {
-		panic(err.Error())
-	}
-
-	aesgcm, err := cipher.NewGCM(block)
-	if err != nil {
-		panic(err.Error())
-	}
-
-	plaintext, err := aesgcm.Open(nil, nonce, ciphertext, nil)
-	if err != nil {
-		panic(err.Error())
-	}
-
-	fmt.Printf("%s\n", plaintext)
-	words := strings.Split(string(plaintext), ",")
-	renterPK = tür.RenterPK(words[0])
-
-	i, err := strconv.ParseInt(words[1], 10, 64)
-	if err != nil {
-		panic(err)
-	}
-	timestamp = i
+//Needed for decrypting the encrypted message from the user (over the mqqt broker)
+func (h *HyperLedger) decryptPayload(renterPK string, encryptedPayload string) (decryptedPayload string) {
 	return
 }
- 
+
+// 	key, _ := hex.DecodeString(renterPK)
+// 	ciphertext, _ := hex.DecodeString(encryptedPayload)
+// 	return
