7dd4d7fab0e7bddad271a1834aebc3a48f52d79e
peter-mueller <peter7995@gmail.com>
2018-06-07 17:25:14 +0200

    add booking id

--- door/opendoormessage.go
+++ door/opendoormessage.go
@@ -5,6 +5,7 @@ import "time"
 type (
 	OpenDoorMessage struct {
 		DoorPublicKey   DoorPublicKey   `json:"doorPublicKey"`
+		BookingID       string          `json:"bookingID"`
 		Timestamp       time.Time       `json:"timestamp"`
 		RenterPublicKey RenterPublicKey `json:"renterPublicKey"`
 	}
