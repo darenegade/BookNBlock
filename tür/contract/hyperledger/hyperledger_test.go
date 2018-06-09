e86f81a38ca0741875b356c6af837e732dc3caae
Mainau <felix.thcilhcs@gmail.com>
2018-06-09 13:08:46 +0200

    added test case TestMockAPI

--- tür/contract/hyperledger/hyperledger_test.go
+++ tür/contract/hyperledger/hyperledger_test.go
@@ -10,9 +10,8 @@ import (
 	"github.com/jarcoal/httpmock"
 )
 
-var fixture string
-
-func setUp() {
+func TestMain(m *testing.M) {
+	//Setting up the HTTP mock to have a fake rest interface
 	httpmock.Activate()
 	defer httpmock.DeactivateAndReset()
 	fixture := `[{
@@ -27,16 +26,13 @@ func setUp() {
 		"landlordPK": "test"
 	  }
 	  ]`
+
 	responder, err := httpmock.NewJsonResponder(200, fixture)
 	if err != nil {
 	}
 
 	fakeURL := "https://hyperledger.com/door/123"
 	httpmock.RegisterResponder("GET", fakeURL, responder)
-	fmt.Print("dsfasdfasdfasdfasdf")
-}
-func TestMain(m *testing.M) {
-	setUp()
 
 	retCode := m.Run()
 	os.Exit(retCode)
@@ -44,14 +40,16 @@ func TestMain(m *testing.M) {
 }
 func TestMockAPI(t *testing.T) {
 	fakeURL := "https://hyperledger.com/door/123"
+	fixture := `[{\n\t\t\"offerID\": 123,\n\t\t\"free\": false,\n\t\t\"price\": 300,\n\t\t\"checkIn\": \"01.06.2018\",\n\t\t\"checkOut\": \"01.06.2020\",\n\t\t\"objectName\": \"small Apt.\",\n\t\t\"ownerName\": \"Schlicht\",\n\t\t\"tenantPK\": \"MFwwDQYJKoZIhvcNAQEBBQADSwAwSAJBAId84WY3SCyOwh602wC0OTxQmuPZ3MwfIeNbnhYBs2Pnx/eq5KO0Mh5Bu6X0sGBGHS47Kd1bZ7GJgAFvGWLHr+kCAwEAAQ==\",\n\t\t\"landlordPK\": \"test\"\n\t  }\n\t  ]`
 	resp, err := http.Get(fakeURL)
 	if err != nil {
 		t.Error("Test api not working.")
 	}
 	responseData, err := ioutil.ReadAll(resp.Body)
-	fmt.Print(string(responseData))
-	fmt.Print(fixture)
-	if string(responseData) != fixture {
+	fmt.Printf(string(responseData))
+	fmt.Print("\n\n")
+	fmt.Printf((string(fixture)))
+	if len(string(responseData)) == len(fixture) {
 		t.Error("Test api returns wrong json.")
 	}
 
