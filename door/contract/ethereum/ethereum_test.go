d4c92f3a9d2844671aa23f26a486b43b634dd778
peter-mueller <peter7995@gmail.com>
2018-07-02 21:26:51 +0200

    cherry pick folder from hyperledgerrestbranch

--- door/contract/ethereum/ethereum_test.go
+++ door/contract/ethereum/ethereum_test.go
@@ -2,8 +2,11 @@ package contract
 
 import (
 	"fmt"
+	"log"
 	"os"
 	"testing"
+
+	"github.com/darenegade/BookNBlock/door"
 )
 
 var ethCon Ethereum
@@ -24,20 +27,39 @@ func setUp() {
 func TestCreateConnection(t *testing.T) {
 
 	ethCon.SetPath("https://rinkeby.infura.io/VhXic4UDRfv5w86p2hq7")
-	ethCon.SetContractAddress("0xEe86D8d8163844517676C918556CDf42310c1671")
+	ethCon.SetContractAddress("0x86f7c1a9fc3a143eB85F2d24Fe3bbE8f8A69dB9c")
 	var connected = ethCon.CreateConnection()
 	if !connected {
 		t.Error("Testing the connection failed.")
 	}
 }
 
-// func TestIsAllowed(t *testing.T) {
-// 	ethCon.setPath("https://rinkeby.infura.io/VhXic4UDRfv5w86p2hq7")
-// 	ethCon.setContractAddress("0xEe86D8d8163844517676C918556CDf42310c1671")
-// 	ethCon.createConnection()
-// 	var allowed = ethCon.IsAllowedAt("Franz", time.Parse(time.RFC822, "01 Jan 15 10:00 UTC"))
-// 	if !allowed {
-// 		t.Error("IsAllowed method failed.")
-// 	}
+func TestIsAllowed(t *testing.T) {
+	ethCon.SetPath("https://rinkeby.infura.io/VhXic4UDRfv5w86p2hq7")
+	ethCon.SetContractAddress("0x86f7c1a9fc3a143eB85F2d24Fe3bbE8f8A69dB9c")
+	ethCon.CreateConnection()
+	//currentTime := big.NewInt(time.Date(2018, time.May, 30, 0, 0, 0, 0, time.UTC).Unix())
+	allowed, err := ethCon.IsAllowedAt(door.BookingID(0), door.RenterPublicKey("0xADF900e582b34EC29DF534e32db6250cf9529FB9"), 1530057600001)
+	if err != nil {
+		log.Fatalf("%v", err)
+	}
+	if !allowed {
+		t.Error("IsAllowed method failed.")
+	}
 
-// }
+}
+
+func TestIsNotllowed(t *testing.T) {
+	ethCon.SetPath("https://rinkeby.infura.io/VhXic4UDRfv5w86p2hq7")
+	ethCon.SetContractAddress("0x86f7c1a9fc3a143eB85F2d24Fe3bbE8f8A69dB9c")
+	ethCon.CreateConnection()
+	//currentTime := big.NewInt(time.Date(2018, time.May, 30, 0, 0, 0, 0, time.UTC).Unix())
+	allowed, err := ethCon.IsAllowedAt(door.BookingID(0), door.RenterPublicKey("0xADF900e582b34EC29DF534e32db6250cf9529FB9"), 1530057500001)
+	if err != nil {
+		log.Fatalf("%v", err)
+	}
+	if allowed {
+		t.Error("IsAllowed method failed.")
+	}
+
+}
