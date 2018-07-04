0d90b28e529eda2c315b75a97ae1702237daa6a0
peter-mueller <peter7995@gmail.com>
2018-07-04 18:21:31 +0200

    validator extended logging

--- door/validator.go
+++ door/validator.go
@@ -21,13 +21,14 @@ type (
 
 func (v *Validator) Handle(m OpenDoorMessage) error {
 	now := time.Now()
-	ok, err := v.ContractInfoer.IsAllowedAt(m.Booking, m.RenterPubkey, now.Unix())
+	ok, err := v.ContractInfoer.IsAllowedAt(m.Booking, m.RenterPubkey, now.UnixNano())
 	if err != nil {
 		log.Println("Failed to confirm contract.")
 		return err
 	}
 	if !ok {
 		log.Printf("Mieter %s was denied to open Door.\n", m.RenterPubkey)
+		log.Printf("Time: %v, TimeUnix: %d, Message: %v\n", now, now.UnixNano(), m)
 		return nil
 	}
 
