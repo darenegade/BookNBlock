1ccf11426293dadaf8a5cb8c0c188027308bc76a
peter-mueller <peter7995@gmail.com>
2018-07-05 14:29:07 +0200

    use ms

--- door/validator.go
+++ door/validator.go
@@ -21,14 +21,14 @@ type (
 
 func (v *Validator) Handle(m OpenDoorMessage) error {
 	now := time.Now()
-	ok, err := v.ContractInfoer.IsAllowedAt(m.Booking, m.RenterPubkey, now.UnixNano()/1000)
+	ok, err := v.ContractInfoer.IsAllowedAt(m.Booking, m.RenterPubkey, now.Unix()*1000)
 	if err != nil {
 		log.Println("Failed to confirm contract.")
 		return err
 	}
 	if !ok {
 		log.Printf("Mieter %s was denied to open Door.\n", m.RenterPubkey)
-		log.Printf("Time: %v, TimeUnix: %d, Message: %v\n", now, now.UnixNano()/1000, m)
+		log.Printf("Time: %v, TimeUnix: %d, Message: %v\n", now, now.Unix()*1000, m)
 		return nil
 	}
 
